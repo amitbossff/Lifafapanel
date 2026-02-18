66aeff89791d4186ad4d2fde1ce83f7deb960e79
amitbossff <amitkr706186@gmail.com>
2026-02-18 17:45:50 +0530

    Delete routes directory

--- routes/lifafaRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const lifafaController = require('../controllers/lifafaController');
-
-router.get('/:code', lifafaController.getLifafaByCode);
-router.post('/claim', lifafaController.claimLifafaPublic);
-
-module.exports = router;
--- routes/toolRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const toolController = require('../controllers/toolController');
-
-router.post('/generate-code', toolController.generateCode);
-router.get('/code/:code', toolController.getCode);
-
-module.exports = router;
